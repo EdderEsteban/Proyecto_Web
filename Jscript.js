47c40308497d03e76476a34d6ebbe18d75077c4e
EdderEsteban <edder709@gmail.com>
2022-05-20 01:32:41 -0300

    Trabajo en Hoja Estadisticas

--- Jscript.js
+++ Jscript.js
@@ -8,23 +8,15 @@
    
 
     var picture;
-    var link;
     var cont=0;
-/*  enlace[0] = <a href="Htal_Cerro.html" target="_blank"></a>;
-    enlace[1] = <a href="Htal_JKoslay.html" target="_blank"></a>;
-    enlace[2] = <a href="Htal_LaPunta.html" target="_blank"></a>;
-    enlace[3] = <a href="Htal_Oeste.html" target="_blank"></a>;
-    enlace[4] = <a href="Htal_Sur.html" target="_blank"></a>;
-*/
+
     //click por derecha
     function derecha(){
         if(cont < imagenes.length -1){
             picture.src = imagenes[cont +1];
-            //link.src = enlace[cont +1];     
             cont++;
         }else {
             picture.src = imagenes[0];
-            //link.src = imagenes[0];
             cont = 1;
         }
     }
@@ -33,7 +25,6 @@
     function izquierda(){
        if(cont > 0){
             picture.src = imagenes[cont -1];
-            
             cont--;
         }else {
             picture.src = imagenes[imagenes.length -1];
@@ -41,8 +32,9 @@
         }
     }
 
-    //Vinculacion de Js con Html
+      //Vinculacion de Js con Html
     function Iniciar(){
         picture = document.getElementById("imgcar");
     }
  //}---------------------------------------------------------------------------------------------------------------------------------- 
+ 
